58f1023c9140a79b83e0f5eff896c05689896ab7
hyfshishen <478340079@qq.com>
2025-10-23 05:01:27 -0500

    updating

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'cuSZp Documentation'
+copyright = '2025, Yafan Huang'
+author = 'Yafan Huang'
 
 release = '0.1'
 version = '0.1.0'
